18e7e0549c90c3ed77d3efc0206db0c3cb30d864
Martin <emailregisterlogin@gmail.com>
2020-03-09 05:04:29 +0000

    Add BNO055

--- main/BNO055.cpp
+++ main/BNO055.cpp
@@ -1,12 +1,37 @@
 #include "BNO055.h"
 
-BNO055::BNO055(int id, uint8_t address) {
+BNO055::BNO055(TwoWire* wire, uint8_t address, int id) {
+  
+  wire = wire;
   id = id;
   address = address;
-  instance = new Adafruit_BNO055(id, address);
+  
+  instance = new Adafruit_BNO055(id, address, wire);
   
   if (!instance->begin()) {
-    // BNO sensor not detected. Check wiring.
-    // while (1) {}
+    Bluetooth::print(FAILURE + "BNO sensor with address=" + String(address, HEX) + ", wire=" + String(int(wire)) + ", id=" + String(id) + " not detected. Check wiring / I2C address");
+  } else {
+    status = true;
+    Bluetooth::print(SUCCESS + "BNO sensor with address=" + String(address, HEX) + ", wire=" + String(int(wire)) + " initialized");
   }
+}
+
+bool BNO055::initialized() {
+  return status;
+}
+
+//bnos[i]->getEvent(&angVelocityData, Adafruit_BNO055::VECTOR_GYROSCOPE);
+//bnos[i]->getEvent(&linearAccelData, Adafruit_BNO055::VECTOR_LINEARACCEL);
+//bnos[i]->getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+//bnos[i]->getEvent(&magnetometerData, Adafruit_BNO055::VECTOR_MAGNETOMETER);
+//bnos[i]->getEvent(&gravityData, Adafruit_BNO055::VECTOR_GRAVITY);
+
+float BNO055::getTemperature() {
+  return instance->getTemp();
+}
+
+Vector3D BNO055::getOrientation() {
+  sensors_event_t data;
+  instance->getEvent(&data, Adafruit_BNO055::VECTOR_EULER);
+  return Vector3D(data.orientation.x, data.orientation.y, data.orientation.z);
 }
--- main/BNO055.h
+++ main/BNO055.h
@@ -1,11 +1,28 @@
 #pragma once
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include "Bluetooth.h"
+#include "Vector3D.h"
         
 class BNO055 {
     private:
-      int id;
-      uint8_t address;
+      
       Adafruit_BNO055* instance;
+      
+      TwoWire* wire;
+      uint8_t address;
+      int id;
+      bool status = false;
+      
+      sensors_event_t angVelocity, linearAccel, accelerometer, magnetometer, gravity;
+      
     public:
-      BNO055(int id, uint8_t address);
+    
+      BNO055(TwoWire* wire, uint8_t address, int id);
+      bool initialized();
+      
+      float getTemperature();
+      Vector3D getOrientation();
 };
