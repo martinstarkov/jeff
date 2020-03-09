a886519e48c72f788459e801e62ce2e361669e1a
Martin <emailregisterlogin@gmail.com>
2020-03-09 05:03:43 +0000

    Simple Vector class for saving and displaying 3d data

---
+++ main/Vector3D.h
@@ -0,0 +1,18 @@
+#pragma once
+
+struct Vector3D { 
+  
+    float x, y, z;
+    
+    Vector3D(float x, float y, float z) : x(x), y(y), z(z) {}
+    
+    String toString() { 
+      return "(" + String(x) + "," + String(y) + "," + String(z) + ")";
+    }
+    Vector3D operator/ (const float num) const {
+      return Vector3D(x / num, y / num, z / num);
+    }
+    Vector3D operator+ (const Vector3D &v) const {
+      return Vector3D(x + v.x, y + v.y, z + v.z);
+    }
+};
