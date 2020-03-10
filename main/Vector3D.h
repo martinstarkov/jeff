eaebfd3375eae2e760a26b14a0fb72c83d48ffea
Martin <emailregisterlogin@gmail.com>
2020-03-10 01:19:56 +0000

    Empty constructor for template objects

--- main/Vector3D.h
+++ main/Vector3D.h
@@ -5,6 +5,7 @@ struct Vector3D {
     float x, y, z;
     
     Vector3D(float x, float y, float z) : x(x), y(y), z(z) {}
+    Vector3D() : x(0), y(0), z(0) {}
     
     String toString() { 
       return "(" + String(x) + "," + String(y) + "," + String(z) + ")";
