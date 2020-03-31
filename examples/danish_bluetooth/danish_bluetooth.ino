ce93834319c01f881436bcc9832a360ce58ef780
Martin <emailregisterlogin@gmail.com>
2020-03-31 19:40:49 +0100

    Serial2 usage

--- examples/danish_bluetooth/danish_bluetooth.ino
+++ examples/danish_bluetooth/danish_bluetooth.ino
@@ -1,40 +1,11 @@
-//code write by Moz for YouTube changel LogMaker360, 27-10-2015
-//code belongs to this video: https://www.youtube.com/watch?v=6jZMJ7DFCY0
 
-#include <SoftwareSerial.h>
-SoftwareSerial BT(0, 1); 
-// creates a "virtual" serial port/UART
-// connect BT module TX to 0
-// connect BT module RX to 1
-// connect BT Vcc to 5V, GND to GND
 void setup()  
 {
   // set the data rate for the SoftwareSerial port
-  BT.begin(9600);
+  Serial2.begin(9600);
   // Send test message to other device
-  BT.println("Hello from Arduino");
 }
-char a; // stores incoming character from other device
 void loop() 
 {
-      BT.println(" Sending data!");
-  if (BT.available())
-  // if text arrived in from BT serial...
-  {
-    a=(BT.read());
-    if (a=='1')
-    {
-      BT.println(" LED on");
-    }
-    if (a=='2')
-    {
-      BT.println(" LED off");
-    }
-    if (a=='?')
-    {
-      BT.println("Send '1' to turn LED on");
-      BT.println("Send '2' to turn LED on");
-    }   
-    // you can add more "if" statements with other characters to add more commands
-  }
+  Serial2.println("Hi");
 }
