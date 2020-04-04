466483cda8df52c0fbe9939582b8e0bd81f41307
Martin <emailregisterlogin@gmail.com>
2020-04-04 20:56:17 +0100

    Added: a few possible unix fixes, BT threading, connection, and reading. Additionally, ability to simulate a BT connection

--- ground/threaded_sequence.py
+++ ground/threaded_sequence.py
@@ -3,6 +3,7 @@ import time
 import queue
 import os
 import sys
+import serial
 
 def clear():
     if os.name == 'nt':
@@ -16,14 +17,16 @@ def flush_input():
         while msvcrt.kbhit():
             msvcrt.getch()
     except ImportError:
-        import sys, termios    #for linux/unix
+        import sys, termios #for linux/unix
         termios.tcflush(sys.stdin, termios.TCIOFLUSH)
 
-try:
-    from msvcrt import kbhit
-except ImportError:
-    import termios, fcntl, sys, os
-    def kbhit():
+def kbfunc():
+    #this is boolean for whether the keyboard has bene hit
+    try:
+        import msvcrt
+        return msvcrt.kbhit()
+    except ImportError: #for linux/unix
+        import termios, fcntl, sys, os
         fd = sys.stdin.fileno()
         oldterm = termios.tcgetattr(fd)
         newattr = termios.tcgetattr(fd)
@@ -41,6 +44,29 @@ except ImportError:
         finally:
             termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
             fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+
+# try:
+#     from msvcrt import kbhit
+# except ImportError:
+#     import termios, fcntl, sys, os
+#     def kbhit():
+#         fd = sys.stdin.fileno()
+#         oldterm = termios.tcgetattr(fd)
+#         newattr = termios.tcgetattr(fd)
+#         newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+#         termios.tcsetattr(fd, termios.TCSANOW, newattr)
+#         oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+#         fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+#         try:
+#             while True:
+#                 try:
+#                     c = sys.stdin.read(1)
+#                     return True
+#                 except IOError:
+#                     return False
+#         finally:
+#             termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+#             fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
 
 # Getch code is a modified version of http://code.activestate.com/recipes/134892/
 # Created by stackoverflow user /2734389/kiri
@@ -87,39 +113,41 @@ class _GetchWindows:
         import msvcrt
         return msvcrt.getwch().encode('utf-8')
 
+bt_usage = True
+
 while True:
     clear()
-    confirmation = input("------------------------- \nConnect to Darwin? (y/n): \n------------------------- \n")
+    confirmation = input("------------------------- \nConnect to Darwin? \n'y' -> yes \n'f' -> yes and fake BT connection, for script testing purposes \n'n' -> no \n------------------------- \n")
     if len(confirmation) > 0:
         if confirmation[0] == "y":
             break
         elif confirmation[0] == "n" or confirmation[0] == "q":
             raise SystemExit
+        elif confirmation[0] == "f":
+            bt_usage = False
+            break
     continue
+
+clear()
 
 getch = _Getch()
 lock = threading.Lock()
 data = False
 stop = False
 jeff = False
+bt_status = False
+bt_complete = False
+bt_read = ""
 cycle = 0
 print_delay = 1 # 0.5 # print output delay, seconds
-arming_delay = 0 # 0.05 # slow down arming speed, seconds
+arming_delay = 0.05 # 0.05 # slow down arming speed, seconds
 raw_data_points = 15 # amount of raw data points being sent by teensy data output
-null_data_message = "not connected"
-
-clear()
-
-for i in range(0,100):
-    while kbhit():
-        if getch().decode("utf-8") == "q":
-            raise SystemExit
-    clear()
-    print("------------------------------------------- \nConnecting to Darwin... (" + str(i) + "%) ('q' to quit) \n-------------------------------------------")
-    time.sleep(arming_delay)
-
-flush_input()
-arming_status = True
+bt_fake_signal = "1;2;3;4;5;6;7;8;9;10;"
+null_data_message = "null"
+undefined_data_message = "unreceived value"
+port = 'COM6'
+baud = 9600
+bt = None
 
 def determine_action(c):
     global stop, data, jeff
@@ -159,17 +187,44 @@ def f_input():
     print("Closing Input Thread")
 
 def process_data_packet():
-    global raw_data_points, null_data_message
-    raw_data = [""] * raw_data_points
-    for i in range(len(raw_data)):
-        if raw_data[i] == "":
-            raw_data[i] = null_data_message
+    global raw_data_points, null_data_message, undefined_data_message
+    data_packet = bt_read
+    if data_packet == "" or data_packet.count(";") == 0:
+        raw_data = [null_data_message] * raw_data_points
+    else:
+        data_packet = data_packet.strip(";").split(";")
+        difference = abs(len(data_packet) - raw_data_points)
+        if difference > 0:
+            raw_data = data_packet
+            for i in range(difference):
+                raw_data.append(undefined_data_message)
+        elif difference < 0:
+            raw_data = []
+            for i in range(raw_data_points):
+                raw_data.append(data_packet[i])
     return raw_data
 
 def print_data():
-    global cycle
+    global cycle, bt_read
     raw_data = process_data_packet()
-    print("Cycle #" + str(cycle) + "\n----------------------------\n" + "Altitude (BMP1): " + raw_data[0] + "\n" + "Altitude (BMP2): " + raw_data[1] + "\n" + "Altitude (BMP3): " + raw_data[2] + "\n" + "Temperature (BMP1): " + raw_data[3] + "\n" + "Temperature (BMP2): " + raw_data[4] + "\n" + "Temperature (BMP3): " + raw_data[5] + "\n" + "Pressure (BMP1): " + raw_data[6] + "\n" + "Pressure (BMP2): " + raw_data[7] + "\n" + "Pressure (BMP3): " + raw_data[8] + "\n" + "Orientation (BNO): " + raw_data[9] + "\n" + "Angular Velocity (BNO): " + raw_data[10] + "\n" + "Linear Acceleration (BNO): " + raw_data[11] + "\n" + "Net Acceleration (BNO): " + raw_data[12] + "\n" + "Gravitational Acceleration (BNO): " + raw_data[13] + "\n" + "Magnetic Field (BNO): " + raw_data[14])
+    data_string = "Cycle #" + str(cycle) + "\n----------------------------\n"
+    data_string += "Raw Bluetooth Read: " + bt_read + "\n"
+    data_string += "Altitude (BMP1): " + raw_data[0] + "\n"
+    data_string += "Altitude (BMP2): " + raw_data[1] + "\n"
+    data_string += "Altitude (BMP3): " + raw_data[2] + "\n"
+    data_string += "Temperature (BMP1): " + raw_data[3] + "\n"
+    data_string += "Temperature (BMP2): " + raw_data[4] + "\n"
+    data_string += "Temperature (BMP3): " + raw_data[5] + "\n"
+    data_string += "Pressure (BMP1): " + raw_data[6] + "\n"
+    data_string += "Pressure (BMP2): " + raw_data[7] + "\n"
+    data_string += "Pressure (BMP3): " + raw_data[8] + "\n"
+    data_string += "Orientation (BNO): " + raw_data[9] + "\n"
+    data_string += "Angular Velocity (BNO): " + raw_data[10] + "\n"
+    data_string += "Linear Acceleration (BNO): " + raw_data[11] + "\n"
+    data_string += "Net Acceleration (BNO): " + raw_data[12] + "\n"
+    data_string += "Gravitational Acceleration (BNO): " + raw_data[13] + "\n"
+    data_string += "Magnetic Field (BNO): " + raw_data[14]
+    print(data_string)
     cycle += 1
 
 def f_output():
@@ -207,22 +262,92 @@ def f_output():
             break
     print("Closing Output Thread")
 
-clear()
+def f_bluetooth():
+    global port, baud, stop, bt, bt_status, bt_read, bt_complete, bt_usage, bt_fake_signal
+    while not stop:
+        if not bt_status:
+            if bt_usage:
+                print("Connecting to Bluetooth... ('q' to quit)")
+                time.sleep(1)
+                try:
+                    bt = serial.Serial(port=port, baudrate=baud)
+                    if bt.is_open:
+                        print("Successfully opened bluetooth serial on " + bt.name)
+                        bt_status = True
+                    else:
+                        print("Could not find " + port)
+                    bt_complete = True
+                except:
+                    print("Bluetooth Initialization Exception Occured")
+                    bt_complete = True
+                    stop = True
+                    break
+            else:
+                bt_complete = True
+                bt_status = True
+                bt_read = bt_fake_signal
+        elif bt_complete and bt_usage:
+            try:
+                response = ""
+                while True:
+                    c = bt.read(1)
+                    if c != b'':
+                        response = str(c).strip("b").strip("'")
+                        while True:
+                            char = bt.read(1)
+                            if char == b'\n':
+                                break
+                            response = response + str(char).strip("b").strip("'")
+                        response = response.replace("\\n", "").replace("\\r", "")
+                        break
+                bt_read = response
+            except:
+                print("Bluetooth Read Exception Occured")
+                stop = True
+                break
+        else: # bluetooth not in use (for testing script)
+            pass
+    print("Closing Bluetooth Thread")
 
 output_thread = threading.Thread(target=f_output, daemon=True)
 input_thread = threading.Thread(target=f_input, daemon=True)
+bluetooth_thread = threading.Thread(target=f_bluetooth, daemon=True)
 
-input_thread.start()
-output_thread.start()
+bluetooth_thread.start()
 
-while True:
-    try:
-        if input_thread.is_alive():
-            continue
-        else:
-            lock.acquire()
+bt_connect_timer = 0
+
+while not bt_complete: # check for 'q' input to quit
+    while kbfunc():
+        if getch().decode("utf-8") == "q":
             raise SystemExit
-    except:
-        input_thread.join()
-        output_thread.join()
-        raise SystemExit
+    #clear()
+    #print("------------------------------------------- \nConnecting to Darwin... (" + str(bt_connect_timer) + " cycles since start) ('q' to quit) \n-------------------------------------------")
+    bt_connect_timer += 1
+    time.sleep(arming_delay)
+
+if bt_status:
+
+    flush_input()
+    arming_status = True
+
+    clear()
+
+    input_thread.start()
+    output_thread.start()
+
+    while True:
+        try:
+            if input_thread.is_alive():
+                continue
+            else:
+                lock.acquire()
+                raise SystemExit
+        except:
+            input_thread.join()
+            output_thread.join()
+            bluetooth_thread.join()
+            raise SystemExit
+
+else:
+    raise SystemExit
