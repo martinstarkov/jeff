f7e2ea3d3ca547734f9d8f0c7b3effb071be58aa
Martin <emailregisterlogin@gmail.com>
2020-06-19 11:56:31 +0100

    Override assert to print information

--- main/common.h
+++ main/common.h
@@ -1,5 +1,21 @@
 #pragma once
+
+#define __ASSERT_USE_STDERR
+
 #include "AllocationMetrics.h"
+
+#include <assert.h>
+
+void __assert(const char *__func, const char *__file, int __lineno, const char *__sexp) {
+    // transmit diagnostic informations through serial link. 
+    Serial.println(__func);
+    Serial.println(__file);
+    Serial.println(__lineno, DEC);
+    Serial.println(__sexp);
+    Serial.flush();
+    // abort program execution.
+    abort();
+}
 
 #define LOG(x) { Serial.println(x); }
 #define LOG_(x) { Serial.print(x); }
