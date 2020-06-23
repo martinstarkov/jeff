6d0ab289bf30f7fc983e46309ebcae3760fb3b4f
Martin <emailregisterlogin@gmail.com>
2020-06-23 13:28:40 +0100

    inline assert

--- main/common.h
+++ main/common.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#define __ASSERT_USE_STDERR
+#include <Arduino.h>
 
 #include "AllocationMetrics.h"
 
+#define __ASSERT_USE_STDERR
+
 #include <assert.h>
 
-void __assert(const char *__func, const char *__file, int __lineno, const char *__sexp) {
+inline void __assert(const char *__func, const char *__file, int __lineno, const char *__sexp) {
     // transmit diagnostic informations through serial link. 
     Serial.println(__func);
     Serial.println(__file);
