c169f977ce6a6bc02884f706038f74726b4758b5
Martin <emailregisterlogin@gmail.com>
2020-06-24 21:56:37 +0100

    Allocate rest of states

--- main/StateMachine.h
+++ main/StateMachine.h
@@ -11,10 +11,14 @@ public:
 	StateMachine() {
 		// add state pointers to state map
 		_states[STATE_STANDBY] = new State(&StandbyState::onEntry, &StandbyState::update, &StandbyState::onExit);
-    _states[STATE_POWERED_ASCENT] = new State(&PoweredAscentState::onEntry, &PoweredAscentState::update, &PoweredAscentState::onExit);
+    	_states[STATE_POWERED_ASCENT] = new State(&PoweredAscentState::onEntry, &PoweredAscentState::update, &PoweredAscentState::onExit);
+    	_states[STATE_COASTING] = new State(&CoastingState::onEntry, &CoastingState::update, &CoastingState::onExit);
+    	_states[STATE_DROGUE_DESCENT] = new State(&DrogueDescentState::onEntry, &DrogueDescentState::update, &DrogueDescentState::onExit);
+    	_states[STATE_MAIN_DESCENT] = new State(&MainDescentState::onEntry, &MainDescentState::update, &MainDescentState::onExit);
+    	_states[STATE_LANDED] = new State(&LandedState::onEntry, &LandedState::update, &LandedState::onExit);
 		
 		// initialize what state the program starts on
-    _currentState = STATE_STANDBY;
+    	_currentState = STATE_STANDBY;
 		_states[_currentState]->onEntry();
 	}
 	void update() {
