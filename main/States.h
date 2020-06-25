297c86aeb10c70c24b66ebae487ac5bd417b2069
Martin <emailregisterlogin@gmail.com>
2020-06-25 12:27:14 +0100

    implement unused functions in headers (necessary for pointers to work)

--- main/States.h
+++ main/States.h
@@ -5,43 +5,37 @@
 class StateMachine;
 
 struct StandbyState {
-    // TODO add array buffer
-    static void onEntry();
-    static void update(StateMachine* stateMachine);
-    static void onExit();
+    static void onEntry() {}
+    static void update(StateMachine* sm);
+    static void onExit() {}
 };
 
 struct PoweredAscentState {
-    // TODO add array buffer
-    static void onEntry();
-    static void update(StateMachine* stateMachine);
-    static void onExit();
+    static void onEntry() {}
+    static void update(StateMachine* sm);
+    static void onExit() {}
 };
 
 struct CoastingState {
-    // TODO add array buffer
-    static void onEntry();
-    static void update(StateMachine* stateMachine);
-    static void onExit();
+    static void onEntry() {}
+    static void update(StateMachine* sm);
+    static void onExit() {}
 };
 
 struct DrogueDescentState {
-    // TODO add array buffer
-    static void onEntry();
-    static void update(StateMachine* stateMachine);
-    static void onExit();
+    static void onEntry() {}
+    static void update(StateMachine* sm);
+    static void onExit() {}
 };
 
 struct MainDescentState {
-    // TODO add array buffer
-    static void onEntry();
-    static void update(StateMachine* stateMachine);
-    static void onExit();
+    static void onEntry() {}
+    static void update(StateMachine* sm);
+    static void onExit() {}
 };
 
 struct LandedState {
-    // TODO add array buffer
-    static void onEntry();
-    static void update(StateMachine* stateMachine);
-    static void onExit();
+    static void onEntry() {}
+    static void update(StateMachine* sm);
+    static void onExit() {}
 };
